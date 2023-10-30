06ff91e6fb0ac6644c3d80c04fb0f2e5dbdc051d
PwnEngine <pwnengine@tuta.io>
2023-10-30 09:03:01 -0700

    generated boilerplate

---
+++ firebase-project/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        
+
+        <Routes>
+          <Route path="/" element={} />
+          <Route path="/login" element={} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
