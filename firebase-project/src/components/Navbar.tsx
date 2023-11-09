167b1b5f07fad799d863c7971abc8a2e4887b2d9
PwnEngine <pwnengine@tuta.io>
2023-11-08 22:42:13 -0800

    finale
    I think this is the finale push I'll make It contains the features I wanted to add. You can create channels to communicate in and react to posts.

--- firebase-project/src/components/Navbar.tsx
+++ firebase-project/src/components/Navbar.tsx
@@ -2,15 +2,13 @@ import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { app_context_type, AppContext } from '../App'
 
-declare type bypass = string | undefined;
-
 const Navbar = () => {
   const { user } = useContext(AppContext) as app_context_type;
 
   return (
     <>
     <div className="navbar">
-      { user && <Link to="/profile"><img className="pfp navbar-pfp" src={user?.photoURL as bypass} width="30" height="30" /></Link> }
+      { user && <Link to="/profile"><img className="pfp navbar-pfp" src={String(user?.photoURL)} width="30" height="30" /></Link> }
       <Link className="nav-item" to="/">Home</Link>
       { user ?
         null : <Link className="nav-item" to="/login">Login</Link>
